2210e7844e36b0038daaba7d07726de4236ce6d0
yash10019coder <lit2020066@iiitl.ac.in>
2021-12-20 15:36:30 +0530

    fixed all bugs in auth api.

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,5 @@
 const express = require('express');
 express().use(express.json());
-const user = require('../models/user');
 const route = express.Router();
 const userSchema = require('../models/user');
 
@@ -8,17 +7,19 @@ route.post("/login", (req, res) => {
     try {
         let email = req.body.email;
         let password = req.body.password;
-        const user = userSchema.findOne({ email: email, password: password });
-        if (user.email === email) {
-            if (user.password === password) {
-                res.json({ "status": 1, "message": "Login Successful" });
+        userSchema.findOne({ 'email': email }, (err, user) => {
+            if (user.email === email) {
+                if (user.password === password) {
+                    res.json({ "status": 1, "message": "Login Successful" });
+                }
+                else {
+                    res.json({ "status": 0, "message": "Invalid Password" });
+                }
+            } else {
+                res.json({ status: 0, "message": "Invalid Email" });
             }
-            else {
-                res.json({ "status": 0, "message": "Invalid Password" });
-            }
-        } else {
-            res.json({ status: 0, "message": "Invalid Email" });
-        }
+
+        });
     } catch (err) {
         res.status(400).json({ "status": 0, "message": err.message });
     }
@@ -27,13 +28,18 @@ route.post("/login", (req, res) => {
 route.post("/signup", (req, res) => {
     try {
         req.id
-        user.create({ name: req.body.name, email: req.body.email, password: req.body.password }, (err, user) => {
-            if (err) {
-                res.json({ "status": 0, "message": err.message });
-            } else {
-                res.json({ "status": 1, "message": "User Created" });
-            }
-        });
+        if (userSchema.find({ 'email': req.body.email })) {
+            res.json({ "status": 0, "message": "Email already exists" });
+        }
+        else {
+            userSchema.create({ name: req.body.name, email: req.body.email, password: req.body.password }, (err, user) => {
+                if (err) {
+                    res.json({ "status": 0, "message": err.message });
+                } else {
+                    res.json({ "status": 1, "message": "User Created" });
+                }
+            });
+        }
     } catch (err) {
         res.status(400).json({ "status": 0, "message": err.message });
     }
